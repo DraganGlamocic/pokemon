4c2ef5910c6ff006401c7680a050b18cc39d2e29
DraganGlamocic <gla.dragan@gmail.com>
2023-11-15 17:55:04 +0100

    added getAttackName. Needs to be more flexible. changed name to pokemonName. Added attackMessage to improve customisation.

--- src/main/java/com/DraganGlamocic/Main.java
+++ src/main/java/com/DraganGlamocic/Main.java
@@ -8,12 +8,12 @@ import com.DraganGlamocic.pokemon.Squirtle;
 public class Main {
     public static void main(String[] args) {
         Pokemon bulbasaur = new Bulbasaur("Bubba");
-//        Pokemon charmander = new Charmander("Chandler");
-//        Pokemon squirtle = new Squirtle("Smelly");
+        Pokemon charmander = new Charmander("Chandler");
+        Pokemon squirtle = new Squirtle("Smelly");
 
         bulbasaur.attack();
-//        charmander.attack();
-//        squirtle.attack();
+        charmander.attack();
+        squirtle.attack();
 
 
     }
