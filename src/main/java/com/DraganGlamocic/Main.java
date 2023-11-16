84d20982a6700a2b7928b8631207607638bea8cc
DraganGlamocic <gla.dragan@gmail.com>
2023-11-16 13:23:46 +0100

    added getAttackName. Needs to be more flexible. changed name to pokemonName. Added attackMessage to improve customisation.

--- src/main/java/com/DraganGlamocic/Main.java
+++ src/main/java/com/DraganGlamocic/Main.java
@@ -4,46 +4,57 @@ import com.DraganGlamocic.pokemon.Bulbasaur;
 import com.DraganGlamocic.pokemon.Charmander;
 import com.DraganGlamocic.pokemon.Pokemon;
 import com.DraganGlamocic.pokemon.Squirtle;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Hey, what is your name?");
+        System.out.println("My name is: ");
+        String playerName = scanner.nextLine();
+        System.out.println("Welcome, " + playerName + "!");
 
         boolean validSelection = false;
         Pokemon chosenPokemon = null;
 
         while (!validSelection) {
-            System.out.println("Choose a starter pokemon: [Bulbasaur] [Charmander] [Squirtle]");
+
+            System.out.println("Please choose a starter pokemon: [Bulbasaur] [Charmander] [Squirtle]");
             System.out.print("Which one would you like: ");
             String chosenPokemonInput = scanner.nextLine();
+            chosenPokemonInput = chosenPokemonInput.toLowerCase();
 
             switch (chosenPokemonInput) {
-                case "Bulbasaur":
+                case "bulbasaur":
                     System.out.print("Give your Bulbasaur a name: ");
                     String bulbasaurName = scanner.nextLine();
                     chosenPokemon = new Bulbasaur(bulbasaurName);
+                    System.out.println("Take good care of " + bulbasaurName + ".");
                     validSelection = true;
                     break;
-                case "Charmander":
+                case "charmander":
                     System.out.print("Give your Charmander a name: ");
                     String charmanderName = scanner.nextLine();
                     chosenPokemon = new Charmander(charmanderName);
                     validSelection = true;
+                    System.out.println("Take good care of " + charmanderName + ".");
                     break;
-                case "Squirtle":
+                case "squirtle":
                     System.out.print("Give your Squirtle a name: ");
                     String squirtleName = scanner.nextLine();
                     chosenPokemon = new Squirtle(squirtleName);
                     validSelection = true;
+                    System.out.println("Take good care of " + squirtleName + ".");
                     break;
                 default:
-                    System.out.println("Invalid pokemon selection. Please try again.");
+                    System.out.println("Hmmm... I don't think we have that pokemon.");
             }
         }
 
         if (chosenPokemon != null) {
-            chosenPokemon.attack();
+            chosenPokemon.attack();     // How to choose an attack? Learn an attack? choose from array or list?
+
         }
     }
 }
